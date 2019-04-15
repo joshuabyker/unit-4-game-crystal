787f2998ef0baeb14c76fc117a10e6ab80a9faa5
joshuabyker <47902849+joshuabyker@users.noreply.github.com>
2019-04-15 15:38:05 -0700

    done ig

--- assets/game.js
+++ assets/game.js
@@ -1,36 +1,46 @@
-var WinningNumber = 100;
-var SoFar=0;
-var Currency;
+var WinningNumber = Math.floor(Math.random()*100);
+var score;
+ var Currency=document.getElementById("Currency");
+ Currency.innerHTML = 0;
+UserClick="click";
+
+
+function Added(val){
+    var increment = parseInt(val);
+    var score = parseInt(Currency.innerHTML);
+    Currency.innerHTML = increment + score;
+}
+$("#WinNumber").text(WinningNumber);
 
 var crystals=$(".crystal");
 for(i=0; i<crystals.length;i++){
     $(crystals[i]).attr("dataValue", Math.floor(Math.random()*12));
-    
 }
  
-
+if(UserClick){
 $(".crystal").on("click",function(){
 console.log(this);
+
 var dataValue= $(this).attr("dataValue");
    console.log(dataValue)
-  
-   alert("You Have Found"+ Currency);
+
+      
+
+   alert("You Have Found"+ dataValue + "Crystals");
+
+
+        var NewValue = $("#Currency")+dataValue; 
+
+
+
+   if (NewValue<WinningNumber){
+   
+}
+   if(NewValue===WinningNumber){
+    alert("Ya Must Have Cheated!");
+}else if( NewValue>WinningNumber){
+    alert("You shoulda cheated better than that")
+}
 })
+}
 
-
-
-// for(var i=0; i<NumberVarries.length;i++){
-// $("#Target").text(WinningNumber);
-// $(".Crystal1").on("click", function(){
-//     console.log("Did Ya click me?");
-// SoFar += count;
-// alert("You've Found" + SoFar);
-
-
-// if(count===WinningNumber){
-//     alert("Ya Must Have Cheated!");
-// }else if( count>=WinningNumber){
-//     alert("Ya gotta cheat better than that!");
-// }
-// })
-// }
